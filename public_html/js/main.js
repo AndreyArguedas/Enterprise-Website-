e8e61deef84e5358d2437d2d6ec43bb7776a6097
Andrey Arguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-03-22 17:48:28 -0600

    Template almost done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,29 @@
+'use strict';
 
+$(function(){
+    
+    var $banSlide =$('#banSlide');
+    var $container = $banSlide.find('.slides');
+    var $slides = $container.find('.slide');
+    var mySlide = 0;
+    var interval;
+    function startBanner() {
+        interval = setInterval(function () {
+            $container.animate({'margin-left': '-=920px'}, 1000, function () {
+                mySlide++;
+                if (mySlide === $slides.length) {
+                    mySlide = 0;
+                    $container.css('margin-left', 0);
+                }
+            });
+        }, 3000);
+    }
+    
+    function pauseBanner(){
+        clearInterval(interval);
+    }
+
+    $banSlide.on('mouseover',pauseBanner).on('mouseleave',startBanner);
+    
+    startBanner();
+});
